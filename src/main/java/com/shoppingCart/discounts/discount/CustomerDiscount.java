42aa9c28e640ee0988cd3180ee80b45e2b1936c2
anupm <anupmehta702@gmail.com>
2020-09-19 14:42:55 +0530

    Updated customerDiscount to abstract class

--- src/main/java/com/shoppingCart/discounts/discount/CustomerDiscount.java
+++ src/main/java/com/shoppingCart/discounts/discount/CustomerDiscount.java
@@ -3,23 +3,17 @@ package com.shoppingCart.discounts.discount;
 import com.shoppingCart.customer.Customer;
 import com.shoppingCart.customer.CustomerType;
 
-public class CustomerDiscount implements Discount {
-    int lowRangeBillAmount = 0;
-    int highRangeBillAmount = 0;
-    int discountPercentage = 0;
-    public CustomerType discountFor;
+public class CustomerDiscount extends Discount {
 
     public CustomerDiscount(int lowRangeBillAmount, int highRangeBillAmount, int discountPercentage,CustomerType discountFor) {
-        this.lowRangeBillAmount = lowRangeBillAmount;
-        this.highRangeBillAmount = highRangeBillAmount;
-        this.discountPercentage = discountPercentage;
-        this.discountFor = discountFor;
+        super(lowRangeBillAmount,highRangeBillAmount,discountPercentage,discountFor);
     }
 
     public int calculateDiscount(Customer customer) {
-
+        isDiscountValidFor(customer);
         return 0;
     }
+
 
     @Override
     public String toString() {
--- src/main/java/com/shoppingCart/discounts/discount/Discount.java
+++ src/main/java/com/shoppingCart/discounts/discount/Discount.java
@@ -1,7 +1,24 @@
 package com.shoppingCart.discounts.discount;
 
 import com.shoppingCart.customer.Customer;
+import com.shoppingCart.customer.CustomerType;
 
-public interface Discount {
-    int calculateDiscount(Customer customer);
+public abstract class Discount {
+    int lowRangeBillAmount = 0;
+    int highRangeBillAmount = 0;
+    int discountPercentage = 0;
+    public CustomerType discountFor;
+
+    public Discount(int lowRangeBillAmount, int highRangeBillAmount, int discountPercentage,CustomerType discountFor) {
+        this.lowRangeBillAmount = lowRangeBillAmount;
+        this.highRangeBillAmount = highRangeBillAmount;
+        this.discountPercentage = discountPercentage;
+        this.discountFor = discountFor;
+    }
+
+    abstract int calculateDiscount(Customer customer);
+
+    protected boolean isDiscountValidFor(Customer customer) {
+        return true;
+    }
 }
